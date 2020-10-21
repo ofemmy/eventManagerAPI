ec367933ed5ac1f82edc8df3b28f4830a4ea3471
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-10-21 04:46:45 +0200

    added basic auth and ownership permissions to user model

---
+++ users/serializer.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "email", "first_name", "last_name", "password"]
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+
+from users.models import User
+from users.permissions import IsUserOrReadOnly
+from users.serializer import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsUserOrReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_admin:
+            raise PermissionDenied(code=403, detail="You have to be admin user to access this resource")
+        return super().list(request, *args, **kwargs)
