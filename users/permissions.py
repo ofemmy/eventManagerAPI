b662094d0591c4198689c3845b037b2cdf003280
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-10-21 05:41:32 +0200

    enabled user creation without need for login

--- users/permissions.py
+++ users/permissions.py
@@ -6,3 +6,16 @@ class IsUserOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+
+class IsAuthenticatedWithCreateExemption(permissions.BasePermission):
+    """
+    only grant permission for creating new resource even without logging in
+    For example I can create a new user without needing to log in
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == "POST":
+            return True
+        else:
+            return False
--- users/views.py
+++ users/views.py
@@ -1,19 +1,23 @@
 # Create your views here.
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import viewsets
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
-from users.permissions import IsUserOrReadOnly
+from users.permissions import IsUserOrReadOnly, IsAuthenticatedWithCreateExemption
 from users.serializer import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsUserOrReadOnly]
+    permission_classes = [IsAuthenticatedWithCreateExemption, IsUserOrReadOnly]
+
 
     def list(self, request, *args, **kwargs):
-        if not request.user.is_admin:
-            raise PermissionDenied(code=403, detail="You have to be admin user to access this resource")
+        """
+        only logged in users and admin users can get full list of users
+        """
+        if isinstance(request.user, AnonymousUser) or not request.user.is_admin:
+            raise PermissionDenied(code=403, detail="You have to be logged in and admin user to access this resource")
         return super().list(request, *args, **kwargs)
