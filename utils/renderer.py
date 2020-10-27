ce519f8f6b8d153c7864430111dd5e305a5c8746
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-10-27 18:58:46 +0100

    refactored customjsonrenderer

--- utils/renderer.py
+++ utils/renderer.py
@@ -3,5 +3,9 @@ from rest_framework.renderers import JSONRenderer
 
 class CustomJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        data = {'data': data}
+        status_code = renderer_context['response'].status_code
+        if str(status_code).startswith("2"):
+            data = {'status': 'success', 'data': data}
+        else:
+            data = {'status': 'error', 'message': data['detail']}
         return super(CustomJSONRenderer, self).render(data, accepted_media_type, renderer_context)
